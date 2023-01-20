2da9becf3b25547769a3c8e3ab569048c2abb554
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-20 15:22:31 -0300

    Global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
