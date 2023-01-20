283c251a6620bcba143b6624c3808ce6f3e2e94c
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-20 15:15:11 -0300

    stiches style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 8,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
+  );
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  css,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: "#8257e6",
+    },
+  },
+});
