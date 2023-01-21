d354aa304fc6657dc4e01c3fd5d7340bec526816
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-21 09:21:56 -0300

    Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import Logo from "../assets/Logo.svg";
+import { Container, Header } from "../styles/pages/app";
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={Logo.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
