f0982925bdb85e7a845f5fc892d9e512e68161cd
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-21 10:01:50 -0300

    Product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,25 @@
-import { styled } from "../styles";
-
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: 8,
-  border: 0,
-  padding: "4px 8px",
-
-  span: {
-    fontWeight: "bold",
-  },
-
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
-
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import camiseta1 from "../assets/Shirt-1.png";
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,62 @@
+import { styled } from "..";
+
+export const HomeContainer = styled("main", {
+  display: "flex",
+  gap: "3rem",
+  width: "100%",
+  marginLeft: "auto",
+  maxWidth: "calc(100vw - ((100vw - 1180px)/2))",
+  minHeight: 656,
+});
+
+export const Product = styled("a", {
+  background: "linear-gradient(180deg, #1ea463 0%, #7465d4 100%)",
+  borderRadius: 8,
+  padding: "0.25rem",
+  cursor: "pointer",
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  overflow: "hidden",
+
+  img: {
+    objectFit: "cover",
+  },
+
+  footer: {
+    position: "absolute",
+    padding: "2rem",
+    bottom: "0.25rem",
+    left: "0.25rem",
+    right: "0.25rem",
+
+    borderRadius: 6,
+
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    backgroundColor: "rgba(0,0,0,0.6)",
+
+    transform: "translateY(110%)",
+    opacity: 0,
+    transition: "all 0.2s ease-in-out",
+
+    strong: {
+      fontSize: "$lg",
+    },
+
+    span: {
+      fontSize: "$xl",
+      fontWeight: "bold",
+      color: "$green300",
+    },
+  },
+  "&:hover": {
+    footer: {
+      transform: "translateY(0%)",
+      opacity: 1,
+    },
+  },
+});
