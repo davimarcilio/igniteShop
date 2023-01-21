4a2bfb42531b9ff4c57a5bef26cf5a6a282a9f97
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-21 09:32:29 -0300

    Next/Image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 import Logo from "../assets/Logo.svg";
 import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={Logo.src} alt="" />
+        <Image src={Logo} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
