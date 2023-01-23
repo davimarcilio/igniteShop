2715f3ff6b2c8e6239c0ff5ba0748cfe084439a8
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-23 15:57:09 -0300

    style product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,26 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter();
-  return <h1>{query.id}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          perspiciatis id, facilis hic eveniet quam enim at cupiditate minima
+          quae voluptate assumenda fugit incidunt amet soluta quasi. Provident,
+          illum officia?
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
