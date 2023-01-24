1a93ca80c257c3e91b0419dbc0cd40b8c8122cac
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-23 21:29:12 -0300

    Success Page

---
+++ src/styles/pages/success.ts
@@ -0,0 +1,51 @@
+import { styled } from "..";
+
+export const SuccessContainer = styled("main", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  margin: "0 auto",
+  height: 656,
+  h1: {
+    fontSize: "$2xl",
+    color: "$gray100",
+  },
+  p: {
+    fontSize: "$xl",
+    color: "$gray300",
+    maxWidth: 560,
+    textAlign: "center",
+    lineHeight: 1.4,
+    marginTop: "2rem",
+  },
+
+  a: {
+    display: "block",
+    marginTop: "5rem",
+    color: "$green500",
+    fontSize: "$lg",
+    textDecoration: "none",
+    fontWeight: "bold",
+    "&:hover": {
+      color: "$green300",
+    },
+  },
+});
+export const ImageContainer = styled("div", {
+  width: "100%",
+  maxWidth: 130,
+  height: 145,
+  background: "linear-gradient(180deg, #1ea463 0%, #7465d4 100%)",
+  borderRadius: 8,
+  padding: "0.25rem",
+  marginTop: "4rem",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  img: {
+    objectFit: "cover",
+  },
+});
