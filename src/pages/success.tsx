6fd9e7be2e71eb25387fc11f35f2502ccbfc9b52
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-23 22:09:11 -0300

    redirect

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -38,6 +38,15 @@ export default function Success({ customerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
